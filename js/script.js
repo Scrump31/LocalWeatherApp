737bedd4b1b8936162dd2b38dea51326b4264325
Scottie Crump <scottiecrump@yahoo.com>
2016-06-15 15:04:28 -0400

    added toggle button group and re-styled layout

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-// Toggle button to change degrees
-$('button').click(function() {
-    $('#dInFar, #dInCel').toggle();
+// Click to show fahrenheit
+$('#c').on('click', function(){
+    $(this).siblings().removeClass('active'); 
+    $(this).toggleClass('active');
+    $('#dInCel').show();
+    $('#dInFar').hide();
 });
-
+// Click to show Celsius
+$('#f').on('click', function(){
+    $(this).siblings().removeClass('active'); 
+    $(this).toggleClass('active');
+    $('#dInCel').hide();
+    $('#dInFar').show();
+});
